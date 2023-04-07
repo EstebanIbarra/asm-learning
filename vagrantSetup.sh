4533f7f40575061be420a3a8af28838f76bd311b
EstebanIbarra <sectorjeat@gmail.com>
2023-04-06 22:28:27 -0600

    Adds debugging tools to Vagrant environment

--- vagrantSetup.sh
+++ vagrantSetup.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 apt update && apt upgrade -y && \
 apt-get update && apt-get upgrade -y && \
-apt-get install -y libc6-dev-i386 make && \
+apt-get install -y \
+    libc6-dev-i386 \
+    make \
+    lldb \
+    gdb && \
 echo "Restarting Packagekit service..." && \
 systemctl restart packagekit.service && \
 echo "Restarted" && \
